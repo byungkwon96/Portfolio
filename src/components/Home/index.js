bcae342a492b0de9257d4a5b765c51f8a8c21fb4
Byung kwon Kim <byungkwon96@gmail.com>
2022-05-17 06:25:35 +0900

    done about page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './index.scss'
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -37,6 +38,7 @@ const Home = () => {
                 <h2>Frontend Developver /.../ ...</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
+            <Logo />
         </div>
     );
 }
